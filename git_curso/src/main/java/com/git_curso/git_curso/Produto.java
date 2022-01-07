c775363edad52394ade45dc55decdf5e59a6bea7
marlei melchior <marleimelchior@gmail.com>
2022-01-07 16:17:56 -0300

    atualização das pastas

---
+++ git_curso/src/main/java/com/git_curso/git_curso/Produto.java
@@ -0,0 +1,30 @@
+package com.git_curso.git_curso;
+
+public class Produto {
+
+    String alho = "alho";
+    String Abobora = "abobora";
+    String cenoura = "cenoura";
+    public String getAlho() {
+        return alho;
+    }
+    public void setAlho(String alho) {
+        this.alho = alho;
+    }
+    public String getAbobora() {
+        return Abobora;
+    }
+    public void setAbobora(String abobora) {
+        Abobora = abobora;
+    }
+    public String getCenoura() {
+        return cenoura;
+    }
+    public void setCenoura(String cenoura) {
+        this.cenoura = cenoura;
+    }
+
+    
+
+
+}
