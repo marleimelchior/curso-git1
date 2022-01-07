b230c8ae2a8b00ccef0be22127ff9afd930de75b
marlei melchior <marleimelchior@gmail.com>
2022-01-07 15:45:05 -0300

    Enviando todos os arquivos

---
+++ git_curso/src/main/java/com/git_curso/git_curso/GitCursoApplication.java
@@ -0,0 +1,13 @@
+package com.git_curso.git_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCursoApplication.class, args);
+	}
+
+}
---
+++ git_curso/src/test/java/com/git_curso/git_curso/GitCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_curso.git_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
